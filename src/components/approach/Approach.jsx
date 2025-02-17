b59c643234fc74027959e75fee03f884ca28f4eb
TeodoraDumitru99 <teodum.1999@gmail.com>
2025-02-17 22:33:29 +0200

    approach-redesign, font size & text change

--- src/components/approach/Approach.jsx
+++ src/components/approach/Approach.jsx
@@ -9,23 +9,25 @@ const Approach = () => {
       id: 0,
       header: "From Passion to Precision",
       text1:
-        " My journey into Frontend Development and UI/UX design started with a love for creating thoughtful, and visually engaging experience, as I have always been passioned about Product Design.",
+        "My journey into Frontend Development and UI/UX design began with a deep appreciation for crafting meaningful Product Design.",
       text2:
-        "Whether it's coding a responsive interface or crafting a seamless user journey, my focus is on turning ideas into intuitive, functional designs that users enjoy interacting with.",
+        "Whether I'm coding a responsive interface, refining microinteractions, or designing a seamless user flow, my goal is to bridge the gap between functionality and design.",
     },
     {
       id: 1,
       header: "Design Meets Development",
-      text1: "I believe in blending creativity with technical precision.",
+      text1:
+        "I believe that great digital experiences are built at the intersection of creativity and technical precision. A well-designed product isn't just visually appealing—it's structured for clarity, accessibility, and scalability.",
       text2:
-        "Every project I take on is an opportunity to bring designs to life with clean, scalable code while ensuring the end product reflects the core values of usability and aesthetics.",
+        "Every project I take on is an opportunity to bring designs to life through clean, efficient code while following principles of usability and aesthetics.",
     },
     {
       id: 2,
       header: "Always Growing",
-      text1: "Staying curious and constantly improving are key to my approach.",
+      text1:
+        "Staying curious and constantly refining my skills is central to my approach as a developer and designer,I am looking to push my creative boundaries.",
       text2:
-        "My goal is to expand my skills with 3D Modeling , as I have prior experience with CAM/CAD Modeling Softwares. I am currently refining my skills in Blender and Autodesk Maya.",
+        "Recently, I've been expanding my expertise in 3D modeling, drawing from my background in CAM/CAD software. I'm currently deepening my skills in Blender and Autodesk Maya, exploring how 3D elements can enhance web experiences and interactive interfaces.",
     },
   ];
 
@@ -38,24 +40,6 @@ const Approach = () => {
       <div className="wrapper">
         <h3 className="approach_title">Approach</h3>
         <div className="approach_content">
-          <div className="approach_text_div">
-            {textContent.map((item, id) => (
-              <div
-                key={id}
-                className={`approach_text_element ${
-                  activeIndex === id ? "active" : ""
-                }${activeIndex !== id ? "hidden" : ""}`}
-              >
-                <h4 className="approach_header">{item.header}</h4>
-                <div className="approach_text">
-                  {item.text1}
-                  <br></br>
-                  {item.text2}
-                </div>
-              </div>
-            ))}
-          </div>
-
           <div className="approach_timeline_div">
             {textContent.map((_, id) => (
               <div
@@ -74,6 +58,24 @@ const Approach = () => {
                   alt="Img"
                   className="approach_circle"
                 />
+              </div>
+            ))}
+          </div>
+
+          <div className="approach_text_div">
+            {textContent.map((item, id) => (
+              <div
+                key={id}
+                className={`approach_text_element ${
+                  activeIndex === id ? "active" : ""
+                }${activeIndex !== id ? "hidden" : ""}`}
+              >
+                <h4 className="approach_header">{item.header}</h4>
+                <div className="approach_text">
+                  {item.text1}
+                  <br></br>
+                  {item.text2}
+                </div>
               </div>
             ))}
           </div>
