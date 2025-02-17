e734709e073176c677df2cfa65582604174536bc
TeodoraDumitru99 <teodum.1999@gmail.com>
2025-02-17 23:51:15 +0200

    approach-padding, font size & text change

--- src/components/approach/Approach.jsx
+++ src/components/approach/Approach.jsx
@@ -9,25 +9,19 @@ const Approach = () => {
       id: 0,
       header: "From Passion to Precision",
       text1:
-        "My journey into Frontend Development and UI/UX design began with a deep appreciation for crafting meaningful Product Design.",
-      text2:
-        "Whether I'm coding a responsive interface, refining microinteractions, or designing a seamless user flow, my goal is to bridge the gap between functionality and design.",
+        "My journey into Frontend Development and UI/UX design began with a deep appreciation for crafting meaningful Product Design. Whether I am coding a responsive interface, refining microinteractions, or designing a seamless user flow, my goal is to bridge the gap between functionality and design.",
     },
     {
       id: 1,
       header: "Design Meets Development",
       text1:
-        "I believe that great digital experiences are built at the intersection of creativity and technical precision. A well-designed product isn't just visually appealing—it's structured for clarity, accessibility, and scalability.",
-      text2:
-        "Every project I take on is an opportunity to bring designs to life through clean, efficient code while following principles of usability and aesthetics.",
+        "I believe that great digital experiences are built at the intersection of creativity and technical precision. A well-designed product isn't just visually appealing—it's structured for clarity, accessibility, and scalability. Every project I take on is an opportunity to bring designs to life through clean, efficient code while following principles of usability and aesthetics.",
     },
     {
       id: 2,
       header: "Always Growing",
       text1:
-        "Staying curious and constantly refining my skills is central to my approach as a developer and designer,I am looking to push my creative boundaries.",
-      text2:
-        "Recently, I've been expanding my expertise in 3D modeling, drawing from my background in CAM/CAD software. I'm currently deepening my skills in Blender and Autodesk Maya, exploring how 3D elements can enhance web experiences and interactive interfaces.",
+        "Staying curious and constantly refining my skills is crucial to my approach as a developer and designer. Recently, I've been expanding my expertise in 3D modeling, drawing from my background in CAM/CAD software. I'm currently deepening my skills in Blender and Autodesk Maya, exploring how 3D elements can enhance web experiences and interactive interfaces.",
     },
   ];
 
