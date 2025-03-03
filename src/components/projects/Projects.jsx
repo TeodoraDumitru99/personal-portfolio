bdbe65ec57968810d68e63d957eafe2727076e71
TeodoraDumitru99 <teodum.1999@gmail.com>
2025-03-03 14:08:55 +0200

    projects-data update, font-size, button width & position change

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,28 +1,33 @@
 import "./projects.css";
 import ZummpathImg from "../assets/Zummpath.png";
+import MiniArcadeImg from "../assets/Mini_Arcade.png";
+import BiasZeroImg from "../assets/Bias_Zero.png";
 
 const Projects = () => {
   const cardsContent = [
     {
       id: 0,
-      title: "Zummpath Bioinformatics 1",
-      desc: "Website for Quantumzyme's Industrial Training Program, using React.js and Tailwind",
-      alt: "Website Image",
-      imgsrc: ZummpathImg,
+      title: "Mini Arcade",
+      desc: "UI/UX Design and Frontend Implementation for personal Mini-Arcade web app with classic games, using React.js and CSS.",
+      alt: "Mini Arcade Website Image",
+      imgsrc: MiniArcadeImg,
+      ahref: "https://arcademini.netlify.app/",
     },
     {
       id: 1,
-      title: "Zummpath Bioinformatics 2",
-      desc: "Website for Quantumzyme's Industrial Training Program, using React.js and Tailwind",
-      alt: "Website Image",
+      title: "Zummpath",
+      desc: "UI/UX Design and Frontend Implementation for Quantumzyme's Industrial Training Program, using React.js and Tailwind.",
+      alt: "Zummpath Website Image",
       imgsrc: ZummpathImg,
+      ahref: "https://zummpathbioinformatics.zummitlabs.com/",
     },
     {
       id: 2,
-      title: "Zummpath Bioinformatics 3",
-      desc: "Website for Quantumzyme's Industrial Training Program, using React.js and Tailwind",
-      alt: "Website Image",
-      imgsrc: ZummpathImg,
+      title: "Bias Zero",
+      desc: "UI/UX Design and User Flow Mapping for Zummmit Infolabs' AI Project - Bias Zero, using Figma and Adobe Photoshop.",
+      alt: "Bias Zero Website Image",
+      imgsrc: BiasZeroImg,
+      ahref: "https://biaszero.zummitlabs.com/",
     },
   ];
   return (
@@ -30,7 +35,7 @@ const Projects = () => {
       <div className="wrapper">
         <h2 className="projects_title">Projects</h2>
         <div className="projects_content">
-          {cardsContent.map(({ id, title, desc, alt, imgsrc }) => (
+          {cardsContent.map(({ id, title, desc, alt, imgsrc, ahref }) => (
             <div className="projects_card" key={id}>
               <img
                 loading="lazy"
@@ -40,10 +45,7 @@ const Projects = () => {
               />
               <h3 className="project_title">{title}</h3>
               <p className="project_desc">{desc}</p>
-              <a
-                href="https://zummpathbioinformatics.zummitlabs.com/"
-                className="project_button"
-              >
+              <a href={ahref} className="project_button">
                 View More
               </a>
             </div>
