ce4f545b986846de776ca05d2826141cfe259c27
TeodoraDumitru99 <teodum.1999@gmail.com>
2025-07-25 17:41:11 +0300

    approach - responsive

--- src/components/approach/Approach.jsx
+++ src/components/approach/Approach.jsx
@@ -15,41 +15,40 @@ const Approach = () => {
             </p>
           </h2>
           <div className="approach_column">
-            <span>ABOUT</span>
-            <div className="approach_point">
+            <div className="approach_point heading3">
               <img
                 className="approach_star"
                 loading="lazy"
                 src={StarPointImg}
                 alt="Star Point Icon"
               />
-              <p>
+              <p className="body_text">
                 Whether I am coding a <b>responsive interface</b> or designing a
-                <b>seamless user flow</b>, my goal is to bridge the gap between
-                <b>functionality and design</b>.
+                <b> seamless user flow</b>, my goal is to bridge the gap between
+                <b> functionality and design</b>.
               </p>
             </div>
-            <div className="approach_point">
+            <div className="approach_point heading3">
               <img
                 className="approach_star"
                 loading="lazy"
                 src={StarPointImg}
                 alt="Star Point Icon"
               />
-              <p>
+              <p className="body_text">
                 Great experiences happen where <b>creativity</b> meets
-                <b> technical precision</b>. A well-designed product is not just
+                <b> technical precision.</b> A well-designed product is not just
                 beautiful - it's <b>clear, accessible, and scalable</b>.
               </p>
             </div>
-            <div className="approach_point">
+            <div className="approach_point heading3">
               <img
                 className="approach_star"
                 loading="lazy"
                 src={StarPointImg}
                 alt="Star Point Icon"
               />
-              <p>
+              <p className="body_text">
                 Staying curious and constantly refining my skills is crucial.
                 I'm currently learning
                 <b> Blender, Autodesk Maya and Backend Tools.</b>
